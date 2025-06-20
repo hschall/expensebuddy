eac42f2751844884ff3829b0896919242e5b7030
Hans Schall <hans.schall@gmail.com>
2025-06-20 17:26:53 -0600

    chart try 2

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,9 +2,6 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.js", preload: true
 pin "chartkick"
-pin "Chart.bundle", to: "chart.js", preload: true
-
-
-
-
