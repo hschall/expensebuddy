adb034f839f6c57e3079ce67eb6ed22ef8192226
Hans Schall <hans.schall@gmail.com>
2025-06-20 18:06:52 -0600

    more chart tests

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -89,7 +89,7 @@ class DashboardController < ApplicationController
       scope = scope.where(person: params[:person]) if params[:person].present? && params[:person] != "Todos"
 
       @monthly_labels << start_date.strftime("%b %Y")
-      @monthly_expenses << (scope.sum(:amount) || 0)
+      @monthly_expenses << (scope.sum(:amount).to_f || 0)
     end
   end
 end
